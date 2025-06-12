b6a3acd11d7156cc3962c38b785a3fd296e53b17
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-06-12 10:37:58 +0300

    Fix modal stylings and layout

--- workspaces/next-js/src/app/nextjs/@modal/(.)new-page/page.tsx
+++ workspaces/next-js/src/app/nextjs/@modal/(.)new-page/page.tsx
@@ -1,7 +1,9 @@
 const Page = () => {
   return (
-    <div className="absolute w-full h-screen bg-slate-700 bg-opacity-80 top-0">
-      Modal
+    <div className="absolute w-full h-full bg-slate-200/20 top-0 left-0 flex justify-center items-center">
+      <div className="w-3xl h-2/4 flex justify-center items-center bg-sky-400">
+        Modal
+      </div>
     </div>
   )
 }

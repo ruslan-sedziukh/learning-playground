96d71e1973c604d78e014c3e01fec4f34052874f
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-07-20 10:58:33 +0300

    Update main layout in next-js

--- workspaces/next-js/src/app/layout.tsx
+++ workspaces/next-js/src/app/layout.tsx
@@ -24,17 +24,19 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-[100%] flex justify-center bg-neutral-50 `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex flex-col justify-start items-center bg-neutral-50`}
       >
-        <div className="flex flex-col items-center h-screen w-full max-w-[1024px]">
-          <header className="flex items-center justify-center py-2 w-full border-b-2 border-neutral-300">
-            <Link href="/">Home</Link>
-          </header>
+        <header className="flex items-center justify-center py-2 w-full border-b-2 border-neutral-300 h-fit">
+          <Link href="/">Home</Link>
+        </header>
 
-          <main className="w-full p-2 max-w-[1024px]">{children}</main>
-        </div>
+        <main className="w-full p-4 overflow-auto flex justify-center">
+          <div className="w-full max-w-[1024px] h-fit">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
